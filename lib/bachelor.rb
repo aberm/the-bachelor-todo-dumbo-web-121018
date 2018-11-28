93c3207fb26df3136940a71a4d6cb75b906d5a28
Menachem Aber <abermendel@gmail.com>
2018-11-28 22:39:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,49 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  
+  data.each do |seasons, info|
+    if seasons == season
+      # binding.pry
+      i = 0
+      while i < info.length
+        if info[i]["status"] == "Winner"
+          return info[i]["name"].split(" ").first.to_s
+        else
+          i += 1
+        end
+      end
+    end
+  end
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |seasons, info|
+    i = 0
+    while i < info.length
+      if info[i]["occupation"] == occupation
+        return info[i]["name"]
+      else
+        i += 1
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |seasons, info|
+    i = 0
+    while i < info.length
+      if info[i]["hometown"] == hometown
+        count += 1
+      else
+        i += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
